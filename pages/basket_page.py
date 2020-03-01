414df3020655c99220ad8eebf00f0f8a99483c0f
Andrey Egorov <andreyegorov1985@gmail.com>
2020-03-01 18:00:14 +0300

    added registration tests, finished project

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -4,7 +4,7 @@ from pages.locators import BasketPageLocators
 
 class BasketPage(BasePage):
     EMPTY_BASKET_MSG = {"ar": "متابعة التسوق", "ca": "La seva cistella està buida.", "cs": "Váš košík je prázdný.",
-                        "da": "Din indkøbskurv er tom.", "de": "Ihr Warenkorb ist leer.",
+                        "da": "Din indkøbskurv er tom.", "de": "Ihr Warenkorb ist leer.", "en": "Your basket is empty.",
                         "en-gb": "Your basket is empty.", "el": "Το καλάθι σας είναι άδειο.",
                         "es": "Tu carrito esta vacío.", "fi": "Korisi on tyhjä", "fr": "Votre panier est vide.",
                         "it": "Il tuo carrello è vuoto.", "ko": "장바구니가 비었습니다.", "nl": "Je winkelmand is leeg",
--- pages/login_page.py
+++ pages/login_page.py
@@ -16,3 +16,13 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL)
+        email_field.send_keys(email)
+        password_field = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD)
+        password_field.send_keys(password)
+        password_confirmation_field = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_CONFIRMATION)
+        password_confirmation_field.send_keys(password)
+        submit_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        submit_button.click()
